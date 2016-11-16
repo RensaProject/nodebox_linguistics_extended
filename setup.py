a04e18352996a823efb79d107517d33b4ea43757
Sarah H <smharmon@ucsc.edu>
2016-11-16 00:24:38 -0800

    add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'nodebox_linguistics_extended',
+  packages = ['nodebox_linguistics_extended'],
+  version = '0.0.1',
+  description = 'Extension of the Nodebox Linguistics library',
+  author = 'Sarah Harmon',
+  author_email = 'smharmon@ucsc.edu',
+  license='GNU',
+  url = 'https://github.com/RensaProject/nodebox_linguistics_extended',
+  classifiers = [
+    'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+  ],
+)
